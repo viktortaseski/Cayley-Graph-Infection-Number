300a58a2c753e134c6c80eb24b5391d89da6b021
Viktor Taseski <vtaseski24@gmail.com>
2025-12-01 20:13:12 +0100

    GUI improvements

--- solution.py
+++ solution.py
@@ -1,16 +1,17 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 
-def check_infected(Graph, infected):
-# 1. INITIAL COLORS: red for initially infected, blue for others
 
+def check_infected(Graph, infected, graph_name="Graph"):
+    # 1. INITIAL COLORS: red for initially infected, blue for others
+    initial_infected = set(infected)
     print(f"{Graph}\tBEFORE infection: {infected}")
     while True:
         newly_infected = set()
 
         for vertex in Graph:
             if vertex in infected:
-             continue
+                continue
             count = 0
             for neighbor in Graph.neighbors(vertex):
                 if neighbor in infected:
@@ -23,11 +24,10 @@ def check_infected(Graph, infected):
         for node in newly_infected:
             Graph.nodes[node]["color"] = "red"
             infected.add(node)
-    
 
     print(f"{Graph}\tAFTER infected:", infected)
     node_colors = ["red" if node in infected else "blue" for node in Graph.nodes()]
 
-    #labels = {node: node + 1 for node in Graph.nodes()}
+    plt.figure(graph_name)
+    plt.title(f"{graph_name}: initial infected {sorted(initial_infected)}")
     nx.draw_circular(Graph, node_color=node_colors, node_size=800, with_labels=True)
-    plt.show()
